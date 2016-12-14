3eea29c8d0d8604f4735d2418c27edb9cf49af5a
peter <peter.von.flocke@gmail.com>
2016-12-14 22:12:37 +0100

    first commit

---
+++ source.js
@@ -0,0 +1,109 @@
+var col = 6; /* x */
+var row = 4; /* y */
+
+var bookmark = new Array();for(var x = 0;x<col;x++){bookmark[x]=new Array();for (var y=0;y<row;y++){bookmark[x][y]={'title':'','url':'','thumb':''};}}
+
+     /* [x][y] */
+bookmark[0][0] = {
+	'title':'',
+	'url':'http://de.pons.com/?l=depl',
+	'thumb':'img/pons.png'
+};
+
+bookmark[1][0] = {
+	'title':'',
+	'url':'http://dict.leo.org/?lang=de&lp=ende',
+	'thumb':'img/leo.png'
+};
+
+bookmark[2][0] = {
+	'title':'',
+	'url':'http://www.linguee.com/english-german/search',
+	'thumb':'img/linguee.png'
+};
+
+bookmark[3][0] = {
+	'title':'',
+	'url':'https://feedly.com/i/my',
+	'thumb':'img/Feedly.png'
+};
+
+bookmark[4][0] = {
+	'title':'',
+	'url':'https://www.reddit.com/',
+	'thumb':'img/reddit.png'
+};
+
+bookmark[5][0] = {
+	'title':'',
+	'url':'http://www.wykop.pl/',
+	'thumb':'img/wykop.png'
+};
+
+bookmark[0][1] = {
+	'title':'',
+	'url':'http://www.ebay.de/myb/WatchList?MyeBay',
+	'thumb':'img/ebay.png'
+};
+
+bookmark[1][1] = {
+	'title':'',
+	'url':'http://amazon.de',
+	'thumb':'img/amazon.png'
+};
+
+bookmark[2][1] = {
+	'title':'',
+	'url':'http://allegro.pl/',
+	'thumb':'img/allegro.png'
+};
+
+bookmark[3][1] = {
+	'title':'',
+	'url':'http://www.teleman.pl/',
+	'thumb':'img/tv.png'
+};
+
+bookmark[4][1] = {
+	'title':'',
+	'url':'https://www.instapaper.com/',
+	'thumb':'img/instapaper.png'
+};
+
+bookmark[5][1] = {
+	'title':'',
+	'url':'http://192.168.0.1',
+	'thumb':'img/fritz.png'
+};
+
+
+bookmark[0][2] = {
+	'title':'',
+	'url':'http://www.wolframalpha.com/',
+	'thumb':'img/WolframAlpha.png'
+};
+
+bookmark[1][2] = {
+	'title':'',
+	'url':'https://cloudpebble.net/',
+	'thumb':'img/pebble.png'
+};
+
+bookmark[2][2] = {
+	'title':'',
+	'url':'http://jsfiddle.net/',
+	'thumb':'img/JSfiddle.png'
+};
+
+bookmark[3][2] = {
+	'title':'',
+	'url':'https://github.com/petervflocke/',
+	'thumb':'img/Octocat.png'
+};
+
+
+bookmark[5][2] = {
+	'title':'',
+	'url':'https://www.o2online.de/mein-o2/dsl/',
+	'thumb':'img/o2.png'
+};
